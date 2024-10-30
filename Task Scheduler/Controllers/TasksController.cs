8a15d6908c823a299e52e4f275f2d7d43fc3721e
Wojciech Rodzeń <wojciech.rodzen@gmail.com>
2024-10-30 23:59:47 +0100

    Dodaj pliki projektów.

---
+++ Task Scheduler/Controllers/TasksController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Task_Scheduler.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        // GET: api/<TasksController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<TasksController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<TasksController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<TasksController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<TasksController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Task Scheduler/Models/Task.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_Scheduler.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime DudDate { get; set; }
+        public string Status { get; set; }
+        public int Priority { get; set; }
+
+
+    }
+}
